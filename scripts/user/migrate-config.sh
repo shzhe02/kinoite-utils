79e5f7c3a5112278ee940f5993c98a1351305caa
Harvey Lim <13660218+shzhe02@users.noreply.github.com>
2024-12-21 01:11:43 +0200

    fixed config migration and removed history file

--- scripts/user/migrate-config.sh
+++ scripts/user/migrate-config.sh
@@ -1,13 +1,15 @@
 # Copying config files for nushell, zellij, helix, and alacritty
 cp -r ../../assets/config/nushell ~/.config/nushell
 cp -r ../../assets/config/zellij ~/.config/zellij
-cp -r ../../assets/cache/starship ~/.cache/starship
+cp -r ../../assets/config/starship ~/.config/starship
 cp -r ../../assets/config/helix ~/.config/helix
 cp -r ../../assets/config/alacritty ~/.config/alacritty
+cp -r ../../assets/cache/starship ~/.cache/starship
 
 # Manage permissions for the pasted files
 chmod -R 700 ~/.config/nushell
 chmod -R 700 ~/.config/zellij
-chmod -R 700 ~/.cache/starship
+chmod -R 700 ~/.config/starship
 chmod -R 700 ~/.config/helix
 chmod -R 700 ~/.config/alacritty
+chmod -R 700 ~/.cache/starship
